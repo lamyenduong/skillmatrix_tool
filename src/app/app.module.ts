e929611546a6d4c7005395f0556e21b84e2449d0
Elysia <lamyenduong.798@gmail.com>
2023-09-04 23:59:22 +0700

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TabViewModule } from 'primeng/tabview';
-
-
+import { RippleModule } from 'primeng/ripple';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,8 +16,11 @@ import { TabViewModule } from 'primeng/tabview';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TabViewModule
+    TabViewModule,
+    RippleModule,
+    BrowserAnimationsModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
