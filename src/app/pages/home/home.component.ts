90ddb82001465e5709b5ca672e0656add3800860
Elysia <lamyenduong.798@gmail.com>
2023-09-14 23:20:09 +0700

    ...

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,7 +13,6 @@ import { TextService } from 'src/app/services/text-service.service';
 export class HomeComponent implements OnInit {
   forms!: Form[]
   homePageText: any
-  displayUploadButton!: boolean
 
   constructor(private formService: FormService, private textService: TextService) { }
 
@@ -23,23 +22,65 @@ export class HomeComponent implements OnInit {
     });
     this.formService.getForms().then(forms => this.forms = forms);
   }
-  //Upload File
+
+  //Upload File  
+  displayUploadButton!: boolean
+  isDragOver = false;
+  fileSelected = false;
+  droppedFile: File | null = null;
   showUploadDialog() {
-    this.displayUploadButton = true
+    this.displayUploadButton = true;
   }
-  changeUploadLabel() {
-    const fileInput = document.querySelector("#file") as HTMLInputElement;;
+  onFileInputChange(event: any): void {
+    const fileInput = event.target;
     if (fileInput.files && fileInput.files.length > 0) {
+      this.fileSelected = true;
       const fileName = fileInput.files[0].name;
       const label = document.querySelector('#fileLabel') as HTMLLabelElement;
       label.innerText = fileName;
+    } else {
+      this.fileSelected = false;
     }
   }
-  onUploadFile() {
+  onFileDrop(event: any): void {
+    event.preventDefault();
+    this.isDragOver = false;
+    const files = event.dataTransfer.files;
+    if (files.length > 0) {
+      this.droppedFile = files[0];
+      const fileName = this.droppedFile?.name;
+      if (fileName) {
+        const label = document.querySelector('#fileLabel') as HTMLLabelElement;
+        label.innerText = fileName;
+        this.fileSelected = true;
+      }
+    }
   }
+  onDragOver(event: any): void {
+    event.preventDefault();
+    this.isDragOver = true;
+  }
+  onDragLeave(event: any): void {
+    event.preventDefault();
+    this.isDragOver = false;
+  }
+  onUploadFile() {
+    if (this.droppedFile) {
+      console.log("Uploading dropped file:", this.droppedFile);
+    } else {
+      const fileInput = document.querySelector("#file") as HTMLInputElement;
+      const filesToUpload = fileInput.files;
+      if (filesToUpload && filesToUpload.length > 0) {
+        const uploadedFile = filesToUpload[0];
+        console.log("Uploading file from input:", uploadedFile);
+      } else {
+        console.log("No file selected for upload.");
+      }
+    }
+  }
+
   //Search
   onSearchChangeHome(event: any) {
     console.log(event.target.value);
   }
-
 }
