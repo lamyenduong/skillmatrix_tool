9dd21874996e626e8bbe37aa73a1624520521ce4
Yến Dương Lâm <72658961+lamyenduong@users.noreply.github.com>
2023-09-22 15:06:55 +0700

    ..

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
+const apiRoute = require("./src/routes/api");
+const { connectToMongoDB, getDb } = require("./src/routes/database");
+
+const app = express();
+const port = process.env.PORT || 8081;
+const host = process.env.HOST;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+apiRoute(app);
+connectToMongoDB();
+
+app.listen(port, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
---
+++ frontend/src/app/services/read-file.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { Form } from '../models/form.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ReadFileService {
+    constructor() { }
+    readFile(file: File): Promise<any[][]> {
+        return new Promise((resolve, reject) => {
+            const reader: FileReader = new FileReader();
+            reader.onload = (e: any) => {
+                const data = new Uint8Array(e.target.result);
+                const workbook = XLSX.read(data, { type: 'array' });
+                const firstSheetName = workbook.SheetNames[0];
+                const worksheet = workbook.Sheets[firstSheetName];
+                const dataArray: any[][] = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+                // const formArray: Form[] = dataArray.map((row: any[]) => {                
+                //   });
+                resolve(dataArray);
+            };
+            reader.onerror = (error) => {
+                reject(error);
+            };
+            reader.readAsArrayBuffer(file);
+        });
+    }
+}
